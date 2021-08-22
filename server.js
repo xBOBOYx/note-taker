433c6e9238154df4a8c5d3ac913b2728d16a1602
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-21 22:24:52 -0700

    write function new note

--- server.js
+++ server.js
@@ -8,8 +8,29 @@ const app = express();
 
 const notes = require('./db/db');
 
+function generateNewNote(body, noteArray) {
+    const genNote = body;
+    noteArray.push(genNote);
+    fs.writeFileSync(
+        path.join(__dirname, 'db/db.json'),
+        JSON.stringify({notes: noteArray}, null, 2)
+    );
+    return noteArray;
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const genNote = generateNewNote(req.body, notes);
+    res.json(genNote);
+
 });
 
 app.listen(PORT, () => {
