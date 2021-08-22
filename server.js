f436094198c6b4af2040010f87e6ba5bbf61860d
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-21 23:00:39 -0700

    updated function

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 const notes = require('./db/db');
 
 function generateNewNote(body, noteArray) {
     const genNote = body;
+    if (!Array.isArray(noteArray)) 
+        noteArray = [];
+
+    if (noteArray.length > 0) 
+        noteArray.push(0);
+        
+        
+    body.id = noteArray.length[0];
+    noteArray[0]++;
     noteArray.push(genNote);
+    
     fs.writeFileSync(
         path.join(__dirname, 'db/db.json'),
         JSON.stringify({notes: noteArray}, null, 2)
@@ -20,17 +29,26 @@ function generateNewNote(body, noteArray) {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+  res.json(notes.slice(1));
 });
 
 app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
     const genNote = generateNewNote(req.body, notes);
     res.json(genNote);
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.listen(PORT, () => {
